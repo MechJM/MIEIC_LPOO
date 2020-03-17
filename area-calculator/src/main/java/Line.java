da2620d583eb4ae41ec091687985b732ca6642c5
MechJM <matosjoaops@protonmail.com>
2020-03-17 17:26:58 +0000

    Minor correction

--- area-calculator/src/main/java/Line.java
+++ area-calculator/src/main/java/Line.java
@@ -13,8 +13,6 @@ public class Line implements Shape {
         this.length = length;
     }
 
-    public double getArea() throws NoAreaException {throw new NoAreaException();}
-
     public void draw() {
         System.out.println("Line");
     }
