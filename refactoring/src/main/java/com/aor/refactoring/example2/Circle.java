39a8f0a857e06abd669815295c581f9a67466e3d
MechJM <matosjoaops@protonmail.com>
2020-03-31 17:13:50 +0100

    Performed the refactoring mentioned in the README

---
+++ refactoring/src/main/java/com/aor/refactoring/example2/Circle.java
@@ -0,0 +1,37 @@
+package com.aor.refactoring.example2;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double x, double y, double radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea()
+    {
+        return Math.PI*Math.pow(radius,2);
+    }
+
+    @Override
+    public double getPerimeter()
+    {
+        return Math.PI * 2 *radius;
+    }
+
+    @Override
+    public void draw(GraphicFramework graphics)
+    {
+        graphics.drawCircle(x, y, radius);
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ refactoring/src/main/java/com/aor/refactoring/example2/README.md
@@ -0,0 +1,5 @@
+## Smells found:
+- Shape is performing the duty of 2 classes (Rectangle and Circle)
+
+## Refactorings done:
+- Separate classes for Rectangle and Circle will be created
